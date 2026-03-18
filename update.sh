6cdc61964b8eff598ed31fe60bfeacf39857d9a4
Chris Peterson <chrispeterson@fastmail.com>
2026-03-18 13:02:36 -0700

    Extract public forge identity into separate config
    Move the user email into .gitconfig-public, included
    conditionally for GitHub and GitLab remotes via
    includeIf hasconfig rules. Update the install script
    to back up both config files before overwriting, and
    tolerate missing files on first run.

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
-mv ~/.gitconfig ~/.gitconfig.old
+mv -f ~/.gitconfig ~/.gitconfig.old 2>/dev/null
+mv -f ~/.gitconfig-public ~/.gitconfig-public.old 2>/dev/null
 cp ./.gitconfig ~/.gitconfig
+cp ./.gitconfig-public ~/.gitconfig-public
